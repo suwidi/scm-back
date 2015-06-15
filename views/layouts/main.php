1ce073d72108bc6917209ed96ec13fd7c1521a67
Suwidi <suwidi@dev.web.id>
2015-06-15 19:48:33 +0700

    fornt page dan footer

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -71,8 +71,8 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-left">&copy; Cubiconia.com <?= date('Y') ?></p>
+        <p class="pull-right">e:info@cubiconia.com|ph. 0811 611 5500</p>
         </div>
     </footer>
 
