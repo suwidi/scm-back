40c5395e0cc7d0b1ca18a24287501a5e4574f591
Suwidi <suwidi@dev.web.id>
2015-07-01 13:24:54 +0700

    layout login

--- views/site/login.php
+++ views/site/login.php
@@ -11,22 +11,22 @@ $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="row">
-            <div class="col-xs-4 col-xs-offset-4 company-form">
-                <div class="h-10"></div>
-                <h2>Identify you here</h2>
-                <div class="h-10"></div>
-              <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>                
-                    <div class="form-group">
-                        <?= $form->field($model, 'username')->textInput(array('placeholder' => 'Email'));  ?>
-                        <?= $form->field($model, 'password')->passwordInput() ?>
-                        <?= $form->field($model, 'captcha')->widget(Captcha::className())->label(false) ?>
-                        <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                    </div>
-                     <div style="text-align:right">                      
-                    &nbsp;&nbsp;&nbsp;&nbsp;     
-                    <?= Html::submitButton("<i class='fa fa-check-circle'></i>&nbsp; Identify Me!", ['class' => 'btn btn-info', 'name' => 'login-button']) ?>
-                    </div>
-                    <div class="h-10"></div>
-     <?php ActiveForm::end(); ?>                  
+    <div class="col-md-4 col-sm-4 col-xs-12 col-md-offset-4 company-form">
+        <div class="h-10"></div>
+        <h2>Identify you here</h2>
+        <div class="h-10"></div>
+        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>                
+            <div class="form-group">
+                <?= $form->field($model, 'username')->textInput(array('placeholder' => 'Email'));  ?>
+                <?= $form->field($model, 'password')->passwordInput(array('placeholder' => 'Password')) ?>
+                <?= $form->field($model, 'captcha')->widget(Captcha::className())->label(false) ?>
+                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            </div>
+            <div style="text-align:right">                      
+                &nbsp;&nbsp;&nbsp;&nbsp;     
+                <?= Html::submitButton("<i class='fa fa-check-circle'></i>&nbsp; Identify Me!", ['class' => 'btn btn-info', 'name' => 'login-button']) ?>
+            </div>
+        <div class="h-10"></div>
+        <?php ActiveForm::end(); ?>                  
     </div>
 </div>
