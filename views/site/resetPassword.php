8d7507ab40baef871f03f202669718d92c342619
Suwidi <suwidi@dev.web.id>
2015-07-01 13:38:02 +0700

    reset password layout

--- views/site/resetPassword.php
+++ views/site/resetPassword.php
@@ -10,20 +10,19 @@ use yii\captcha\Captcha;
 $this->title = 'Reset password';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-reset-password">
-
-    <p>Please choose your new password:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
+<div class="row">
+    <div class="col-md-4 col-sm-4 col-xs-12 col-md-offset-4 company-form">
+        <div class="h-10"></div>
+       <h4>Please choose your new password:</h4>
+        <div class="h-10"></div>   
             <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>            
                 <?= $form->field($model, 'password')->passwordInput() ?>
                 <?= $form->field($model, 'repeatpassword',['inputOptions' => ['placeholder' => 'Retype Password',],])->passwordInput()->label(false) ?>
                 <?= $form->field($model, 'captcha')->widget(Captcha::className())->label(false) ?>
                 <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+                    <?= Html::submitButton('Save', ['class' => 'btn btn-info']) ?>
                 </div>
             <?php ActiveForm::end(); ?>
         </div>
-    </div>
 </div>
+
